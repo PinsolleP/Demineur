be90d8dd6b08675b3833216daf1b2f62d965b206
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-07 09:23:41 +0200

    Déclaration des variables et méthode createGrid

--- src/Demineur.java
+++ src/Demineur.java
@@ -1,2 +1,27 @@
-public class Demineur { 
-}
+public class Demineur {
+    public static int LINES = 6;
+    public static int COLUMNS = 12;
+
+    public static boolean[][] bombGrid;
+
+    public static int[][] bombCount;
+
+    public static boolean[][] revealedBox;
+
+    public static boolean[][] flags;
+
+
+    public static void createGrid() {
+
+        bombGrid = new boolean[LINES][COLUMNS];
+        bombCount = new int[LINES][COLUMNS];
+        revealedBox = new boolean[LINES][COLUMNS];
+        flags = new boolean[LINES][COLUMNS];
+    }
+
+
+
+    public static void main (String[]args){
+
+            }
+        }
