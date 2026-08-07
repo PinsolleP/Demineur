ac82c22147e40466240d239d6111295172f5205e
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-07 10:36:24 +0200

    Ajout méthode displaygrid

--- src/Demineur.java
+++ src/Demineur.java
@@ -19,9 +19,39 @@ public class Demineur {
         flags = new boolean[LINES][COLUMNS];
     }
 
+    public static void displayGrid(){
+        System.out.print("  ");
+        for ( int j = 0; j < COLUMNS; j ++){
+            System.out.print (j + " ");
+        }
+        System.out.println();
+
+        for ( int i = 0; i < LINES; i++){
+            System.out.print (i + " ");
+            for ( int j = 0; j < COLUMNS; j++){
+                if (flags[i][j]){
+                    System.out.print ("F ");
+                } else if (!revealedBox[i][j]) {
+                    System.out.print ("¤ ");
+                } else if (bombGrid[i][j]) {
+                    System.out.print ("X ");
+                }
+                else{
+                    System.out.print (bombCount[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void plantedBomb(boolean bombGrid[][]){
+        
+    }
 
 
     public static void main (String[]args){
-
+        createGrid();
+        displayGrid();
             }
         }
